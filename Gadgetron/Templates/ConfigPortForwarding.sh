2ba78b6548ee68fe35f64b4d1919352726f5e75f
ExchMaster <jingram@microsoft.com>
2015-12-02 14:54:43 -0500

    dos2unix on port forwarding shell script

--- Gadgetron/Templates/ConfigPortForwarding.sh
+++ Gadgetron/Templates/ConfigPortForwarding.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 iptables -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --dport 9002 -j ACCEPT
